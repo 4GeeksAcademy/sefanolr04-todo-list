7062b326957a5f001c2bf4a2dd1dcc02410dbf7f
Stefanolr04 <136249681+Stefanolr04@users.noreply.github.com>
2023-09-19 19:06:43 +0000

    list todo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,49 +1,67 @@
 
-import React , {useState} from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function App() {
+    const [task, setTask] = useState('');       // Estado para el valor actual del input
+    const [tasksList, setTasksList] = useState([]); // Estado para la lista de tareas
 
-//create your first component
-const Home = () => {
-	const [inputValue, setInputValue] = useState("")
-	const [todos, setTodos] = useState([])
-	return (
-		<div className="container">
-			<h1>My todos</h1>
-			<ul>
-				<li>
-					<input
-					type="text"
-					onChange={(e) => setInputValue(e.target.value)} 
-					value={inputValue}
-					onKeyDown={(e) => {
-						if (e.key === "Enter"){
-							setTodos(todos.concat([inputValue] ))
-							setInputValue("")
-						}
-					} }
-					placeholder="What do you need to do?"></input>
-				</li>
-				{todos.map((item, index) => (
-					<li>
-					{item}<i class="fas fa-trash-alt" 
-					onClick={()=>
-		                setTodos(
-						    todos.filter(
-							   (t, currentIndex)=> 
-							     index != currentIndex
-						    )
-					    )
-				    }></i>
-				    </li>
-				))}
-				
-			</ul>
-			<div>{todos.length}</div>
-			
-		</div>
-	);
-};
+    const handleAddTask = (event) => {
+        if (task && event.key == "Enter") {
+            setTasksList([...tasksList, task]); // Añade la tarea actual a la lista
+            setTask('');                        // Limpia el input
+        }
+    };
 
-export default Home;
+    const taskChange = (event) => {
+        setTask(event.target.value)
+    }
+
+    const deleteTask = (index) => {
+        const newList = tasksList.filter((value, i) => i !== index)
+        setTasksList(newList)
+    }
+
+    return (
+        <div className="container">
+            <div className="row">
+                <h1 className='title'>Todo List</h1>
+                <div className='col-12 p-0'>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Nueva tarea"
+                        value={task}
+                        onChange={e => taskChange(e)}
+                        onKeyUp={e => handleAddTask(e)}
+                    />
+                </div>
+                <ul className="list-group col-12 p-0">
+                    {tasksList.map((task, index) => (
+                        <li
+                            key={index}
+                            className="list-group-item d-flex justify-content-between">
+                            <span>{task}</span>
+                            <button
+                                onClick={e => deleteTask(index)}
+                                style={{ border: 'none', background: 'none', padding: 0, cursor: 'pointer' }}
+                            >
+                                <i class="far fa-trash-alt"></i>
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+                {tasksList.length > 0 ? (
+                    <div>
+                        {tasksList.length} item{tasksList.length === 1 ? '' : 's'} left
+                    </div>
+                ) : (
+                    <div>
+                        No hay tareas, añadir tareas
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default App;
